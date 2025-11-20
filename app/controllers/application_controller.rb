be2726e284d49d93bc66a37c7738036749985d98
Krishna <libraryismyparadise@gmail.com>
2025-11-20 21:58:11 +0900

    Add set_current_shop method to manage current shop based on subdomain or shop_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+	before_action :set_current_shop
+
+	private
+
+	def set_current_shop
+		shop = nil
+
+		if request.respond_to?(:subdomain) && request.subdomain.present? && request.subdomain != 'www'
+			shop = Shop.find_by(subdomain: request.subdomain)
+		end
+
+		shop ||= Shop.find_by(id: params[:shop_id]) if params[:shop_id].present?
+		shop ||= Shop.first
+
+		ActsAsTenant.current_tenant = shop
+		@current_shop = shop
+	end
 end
+
